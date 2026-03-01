69abb069e6b62bd000c40b0221fdd9520e9230cb
codesche <codesche@gmail.com>
2026-03-01 16:10:35 +0900

    [20260301] 도서 관리 시스템 실습 - Member

---
+++ src/theory/book/Member.java
@@ -0,0 +1,21 @@
+package theory.book;
+
+public class Member {
+
+    private final String memberId;
+    private final String name;
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public String getName() {
+        return getName();
+    }
+
+}
