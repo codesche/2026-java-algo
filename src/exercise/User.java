eb1c09b5c51995111bcd53f7e14b58fb83de799b
codesche <codesche@gmail.com>
2026-03-09 23:47:11 +0900

    [20260309] MVC 패턴 실습 - entity

---
+++ src/exercise/User.java
@@ -0,0 +1,32 @@
+package exercise;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private int age;
+
+    public User(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", name='" + name + "', age=" + age + "}";
+    }
+
+}
