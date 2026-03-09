7957a36d17f46d1a6c228480d5de8841dd2b03be
codesche <codesche@gmail.com>
2026-03-09 23:47:25 +0900

    [20260309] MVC 패턴 실습 - repository

---
+++ src/exercise/UserRepository.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserRepository {
+
+    private Map<Long, User> store = new HashMap<>();
+
+    public void save(User user) {
+        store.put(user.getId(), user);
+    }
+
+    public User findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
