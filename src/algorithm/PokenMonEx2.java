b55cc083442be0bd71848acd0d2ba08613cdda8a
codesche <codesche@gmail.com>
2026-03-13 20:56:32 +0900

    [20260313] 폰켓몬 - HashSet (다른 풀이)

---
+++ src/algorithm/PokenMonEx2.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+/**
+ * Stream을 활용한 폰켓몬 풀이
+ */
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PokenMonEx2 {
+
+    public int solution(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.toSet());
+
+        return Math.min(set.size() / 2, nums.length);
+    }
+
+}
