588c9b573ab1c88904f7fed549ab852934be6ac8
codesche <codesche@gmail.com>
2026-03-13 21:02:25 +0900

    [20260313] 완주하지 못한 선수 - HashMap(다른 풀이 - hashcode 합 활용)

---
+++ src/algorithm/InCompleteRun2.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.*;
+
+public class InCompleteRun2 {
+    public String solution(String[] participant, String[] completion) {
+        Map<Integer, String> map = new HashMap<>();
+        int hashSum = 0;        // 해시값의 합
+
+        for (String p : participant) {
+            int hash = p.hashCode();
+            map.put(hash, p);
+            hashSum += hash;
+        }
+
+        for (String c : completion) {
+            hashSum -= c.hashCode();
+        }
+
+        return map.get(hashSum);
+    }
+}
