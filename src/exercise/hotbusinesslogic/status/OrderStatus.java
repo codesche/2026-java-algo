41e7f4b59e10e431ed4193e4691f5c516c7fa20b
codesche <codesche@gmail.com>
2026-03-28 17:07:57 +0900

    [20260328] 비즈니스 로직 훈련 - 각 도메인별 상태값 적용 위한 enum 객체

---
+++ src/exercise/hotbusinesslogic/status/OrderStatus.java
@@ -0,0 +1,8 @@
+package exercise.hotbusinesslogic.status;
+
+/**
+ * 주문 상태
+ */
+public enum OrderStatus {
+    CREATED, COMPLETED, FAILED
+}
---
+++ src/exercise/hotbusinesslogic/status/PaymentStatus.java
@@ -0,0 +1,8 @@
+package exercise.hotbusinesslogic.status;
+
+/**
+ * 결제 상태
+ */
+public enum PaymentStatus {
+    SUCCESS, FAIL
+}
